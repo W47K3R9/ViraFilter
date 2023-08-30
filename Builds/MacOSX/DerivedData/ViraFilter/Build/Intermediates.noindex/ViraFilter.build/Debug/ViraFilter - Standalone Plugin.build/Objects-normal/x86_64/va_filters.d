821e93ffbaa3bb9d86f0984ec81b9920b7a70410
Lucas Scheidt <lucas.scheidt@hm.edu>
2023-08-30 20:09:20 +0200

    Builds folder soll weg

--- Builds/MacOSX/DerivedData/ViraFilter/Build/Intermediates.noindex/ViraFilter.build/Debug/ViraFilter - Standalone Plugin.build/Objects-normal/x86_64/va_filters.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/Lucas/Software_Projects/JUCEProjects/ViraFilter/Source/va_filters.cpp \
-  /Users/Lucas/Software_Projects/JUCEProjects/ViraFilter/Source/va_filters.hpp \
-  /Users/Lucas/Software_Projects/JUCEProjects/ViraFilter/Source/wave_elements.hpp
